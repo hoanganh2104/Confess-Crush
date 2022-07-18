750b726a8dcde28197dce113aaf1fc7f351ef672
underword <109515404+hoanganh2104@users.noreply.github.com>
2022-07-18 16:37:23 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     titleWeb: "Gửi cho người tôi yêu",
-    introTitle: 'Tên người yêu bạn',
+    introTitle: 'Trần Võ Tư Tỷ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
